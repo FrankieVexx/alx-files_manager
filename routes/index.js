c81a6186530435d1e1af395354adfa3304c71083
myrajarenga <myrajarenga1234@gmail.com>
2023-09-05 09:05:28 -0700

    routes

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+// routes/index.js
+const express = require('express');
+//const AppController = require('../controllers/AppController');
+const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
+
+const router = express.Router();
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+
+module.exports = router;
